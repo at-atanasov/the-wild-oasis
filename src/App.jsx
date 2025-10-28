dcdc24736adcc89989581e4ece04091dbce8a830
at-atanasov <at.atanasov@gmail.com>
2025-10-28 21:51:11 +0200

    Global Styles With Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Button></Button>
+      <GlobalStyles />
+      <div>
+        <Button>Hello World</Button>
+        <Input />
+      </div>
+      ;
+    </>
+  );
 }
 
 export default App;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,5 +1,6 @@
-import styled, { css } from "styled-components";
-
+//import styled, { css } from "styled-components";
+import styled from "styled-components";
+/*
 const sizes = {
   small: css`
     font-size: 1.2rem;
@@ -20,7 +21,7 @@ const sizes = {
   `,
 };
 
-const variations = {
+ const variations = {
   primary: css`
     color: var(--color-brand-50);
     background-color: var(--color-brand-600);
@@ -47,3 +48,21 @@ const variations = {
     }
   `,
 };
+*/
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-50);
+  box-shadow: var(--shadow-sm);
+  cursor: pointer;
+
+  &:hover {
+    background-color: var(--color-brand-700);
+  }
+`;
+
+export default Button;
