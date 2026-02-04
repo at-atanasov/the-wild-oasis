389e4dafe81351578ea3328c41612b5be7719226
at-atanasov <at.atanasov@gmail.com>
2026-02-04 22:21:52 +0200

    Final Touches + Fixing Bugs

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -4,7 +4,10 @@ import { useLocalStorageState } from "../hooks/useLocalStorageState";
 const DarkModeContext = createContext();
 
 function DarkModeProvider({ children }) {
-  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches,
+    "isDarkMode",
+  );
 
   useEffect(
     function () {
@@ -16,7 +19,7 @@ function DarkModeProvider({ children }) {
         document.documentElement.classList.add("light-mode");
       }
     },
-    [isDarkMode]
+    [isDarkMode],
   );
 
   function toggleDarkMode() {
